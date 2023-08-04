b0a8ef89095596f6645c34b2ff690158a4871de5
Steve <111431635+SteveYu000@users.noreply.github.com>
2023-08-05 02:48:53 +0800

    修改为命令行模式

--- 计算质数/计算质数.cpp
+++ 计算质数/计算质数.cpp
@@ -6,87 +6,73 @@
 #include <fstream>
 #include <windows.h>
 #include <limits>
+#include <string>
 using namespace std;
 
-void Judge_Prime() {
-		int counter = 0;  // 素数的个数
-		long long int num = 0;  // 输入的整数
-		cout << "输入一个整数：";
-		while (1) {
-			cin >> num;
-			if (cin.fail()) {
-				cout << "输入有误，必须为整数！\n请重新输入" << endl;
-				cin.clear(); // 清除错误标志
-				cin.ignore((numeric_limits<streamsize>::max)(), '\n'); // 忽略无效输入
-			}
-			else {
-				break;
-			}
-		}
-		
+void Judge_Prime(long long int num) {
+		bool counter = 0;  // 素数判断
 		for (long long int i = 2; i < num; i++) {
 			if (num % i == 0) {
-				counter++;  // 素数个数加1
+				counter=!counter;  // 不是素数
 				break;
 			}
 		}
 
 		if (counter == 0) {
-			MessageBox(NULL, TEXT("是质数"), TEXT("判断结果"), MB_OK);
+			cout << "是质数" << endl;
 		}
 		else {
-			MessageBox(NULL, TEXT("不是质数"), TEXT("判断结果"), MB_OK);
+			cout << "不是质数" << endl;
 		}
-		system("cls");
 }
 void Calculate_Prime() {
-	long long int pri_Num = 0, counter = 0;       				//pri_Num为要判断的数，从2开始；counter为pri_Num的因数个数
-	ifstream pri_Num_log("质数.log");
+	long long int pri_Num = 0;
+	bool counter = 0;       				//pri_Num为要判断的数，从2开始；counter为pri_Num的因数个数
+	fstream pri_Num_log;
+	pri_Num_log.open("质数.log", ios::in);
 	pri_Num_log >> pri_Num;
 	pri_Num_log.close();
 	while (1) {
 		for (long long int i = 2; i < pri_Num; i++) { 		//如果为2，则直接输出，不进入循环
 			if (pri_Num % i == 0) {				//判断pri_Num是否有除本身之外大于2的因数
-				counter++;
-				continue;				//如果有则 因数个数 加1
+				counter=!counter;
+				break;				//如果有则 因数个数不为0
 			}
 		}
 		if (counter == 0 && pri_Num != 0 && pri_Num != 1) {
 			cout << pri_Num << endl;		//当counter为0时，pri_Num为质数
-			ofstream ouf(".\\质数.log");
-			ofstream fout(".\\质数.log");
-			fout << pri_Num << endl;
-			fout.close();
+			fstream pri_Num_log;
+			pri_Num_log.open(".//质数.log", ios::out);
+			pri_Num_log << pri_Num << endl;
+			pri_Num_log.close();
 		}
-		pri_Num += 1;						/*(1)当a为质数，输出之后加1
-									(2)当a为和数，直接加1*/
-		counter = 0;						//注意!把b初始化
+		pri_Num += 1;						
+		counter = 0;						
 	}
 }
-int main() {
-	while (1) {
-		cout << "这是一个计算和判断质数的程序" << endl;
-		cout << "现有2种功能：\n（1）判断一个数是否为质数\n（2）从零或之前的存档开始计算之后的质数\n请输入1或2以继续,输入exit退出程序：" << endl;
-		string start;
-		cin >> start;
-		if (start == "1" || start == "2") {
-			system("cls");
-			if (start == "1") {
-				Judge_Prime();
+int main(int argc,char *argv[]) {
+		if (argc == 1) {
+			cout << "请输入参数\n参数格式：功能\t[数值]\npri\t数值\t判断输入的数是否为质数\ncal\t从零或之前的存档开始计算之后的质数\nexit\t退出程序" << endl;
+		}
+		else {
+			string func = argv[1];
+			if (func == "pri"&&argc==3) {
+				try {
+					long long int key = stoll(argv[2]);
+					Judge_Prime(key);
+				}
+				catch(const invalid_argument &e){
+					cout << "无效的数值：" << e.what() << endl;
+					cin.clear();
+					cin.ignore((numeric_limits<streamsize>::max)(), '\n');
+				}
 			}
-			if (start == "2") {
-				void Calculate_Prime();
+			else if (func == "cal") {
+				Calculate_Prime();
+			}
+			else {
+				cout << "未定义的操作，请正确输入参数\n参数格式：功能\t[数值]\npri\t数值\t判断输入的数是否为质数\ncal\t从零或之前的存档开始计算之后的质数\nexit\t退出程序" << endl;
 			}
 		}
-		if (start == "exit") {
-			break;
-		}
-		else {//错误输入处理
-			cin.clear();
-			cin.ignore((numeric_limits<streamsize>::max)(), '\n');
-			system("cls");
-			cout << "输入无效，请重新输入" << endl;
-		}
-	}		
 	return 0;
 }
